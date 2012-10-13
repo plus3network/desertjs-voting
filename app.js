481782305095c5cc33a2b33c956f9659a15b51d6
Chris Cowan <me@chriscowan.us>
2012-10-13 14:02:10 -0700

    Movign to less adn hogan

--- app.js
+++ app.js
@@ -3,23 +3,25 @@
  * Module dependencies.
  */
 
-var express = require('express'),
-var routes = require('./routes'),
-var user = require('./routes/user'),
-var http = require('http'),
-var path = require('path');
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , socketio = require('socket.io');
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'hjs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
